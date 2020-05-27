e7237a62f9ce87d570b8da86d0e423a51e7e78e9
joachim Bourquardez <j.bourquardez@laposte.net>
2020-05-27 17:32:54 +0100

    ajout dun nvx host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
 
   config.hosts << "phonealchemist.herokuapp.com"
+  config.hosts << "slasher.store"
 
   config.action_mailer.default_url_options = { host: "phonealchemist.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
